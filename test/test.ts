a0f6e30b257e9ee5dfd38fc7dd60a59e92d41ea5
alny <nygaard14@gmail.com>
2020-03-31 09:43:01 +0200

    Increase Phone Lines tests

--- test/test.ts
+++ test/test.ts
@@ -2,13 +2,26 @@ const assert = require("chai").assert;
 const expect = require("chai").expect;
 const { Purchase } = require("../src/purchase.ts");
 
-var purchase = new Purchase();
-
+// Unit tests
 describe("Including the Internet connection ", () => {
+  var purchase = new Purchase();
   it("should return price 200, when input is true", () => {
     assert.equal(purchase.setInternetConnection(true), 200);
   });
   it("should return price 0, when input is true false", () => {
     assert.equal(purchase.setInternetConnection(false), 0);
+  });
+});
+describe("Increase Phone Lines", () => {
+  it("should return price 150, when 1 phone line has been selected", () => {
+    var purchase = new Purchase();
+    assert.equal(purchase.increasePhoneLines(), 150);
+  });
+  it("should return price 1200, when 8 phone lines has been selected", () => {
+    var purchase = new Purchase();
+    for (let index = 0; index < 7; index++) {
+      purchase.increasePhoneLines();
+    }
+    assert.equal(purchase.increasePhoneLines(), 1200);
   });
 });
